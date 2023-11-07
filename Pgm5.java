7ff789a0a7405472a2cd7783c6266c7909784fe6
Jessin-Sunny <119120870+Jessin-Sunny@users.noreply.github.com>
2023-11-07 19:32:26 +0530

    Add files via upload

---
+++ Pgm5.java
@@ -0,0 +1,55 @@
+abstract class Shape                //abstract class Shape
+{
+    abstract int numberOfSides();   //abstract method
+}
+class Rectangle extends Shape
+{
+    private int sides=4;
+    int numberOfSides()
+    {
+        return sides;
+    }
+    void display()
+    {
+        System.out.println("Sides of a Rectangle : "+sides);
+    }
+}
+class Triangle extends Shape
+{
+    private int sides=3;
+    int numberOfSides()
+    {
+        return sides;
+    }
+    void display()
+    {
+        System.out.println("Sides of a Triangle : "+sides);
+    }
+}
+class Hexagon extends Shape
+{
+    private int sides=6;
+    int numberOfSides()
+    {
+        return sides;
+    }
+
+    void display()
+    {
+        System.out.println("Sides of a Hexagon : "+sides);
+    }
+}
+
+public class Pgm5
+{
+    public static void main(String[] args)
+    {
+        Rectangle r=new Rectangle();
+        Triangle t=new Triangle();
+        Hexagon h=new Hexagon();
+        r.display();
+        t.display();
+        h.display();;
+
+    } 
+}
