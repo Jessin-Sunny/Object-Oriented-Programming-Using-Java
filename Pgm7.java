37e6855d363f42ca4f425cd8c247433a94c6b8ad
Jessin Sunny <119120870+Jessin-Sunny@users.noreply.github.com>
2023-11-20 23:14:27 +0530

    Add files via upload

---
+++ Pgm7.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Pgm7 
+{
+    public static void main(String args[]) 
+    {
+        try
+        {
+            // Writing to a file using FileWriter
+            FileWriter writer = new FileWriter("output.txt");
+            writer.write("This is a sample text.\nWe are writing it to a file using FileWriter.\n");
+            writer.close();
+            System.out.println("Successfully wrote to the file.");
+
+            // Reading from a file using FileReader
+            FileReader reader = new FileReader("output.txt");
+            int character;
+            while ((character = reader.read()) != -1) 
+            {
+                System.out.print((char) character);
+            }
+            reader.close();
+        } 
+        catch (IOException ex) 
+        {
+            System.out.println("An error occurred."+ex);
+        }
+    }
+}
+
